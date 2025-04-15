ede8ed734c77cb190ac1fd7aea3f0474a37f00fa
Sydney Leibowitz <sydneyleibo1@gmail.com>
2025-04-15 16:20:15 -0400

    integer

--- Weather1.py
+++ Weather1.py
@@ -25,7 +25,7 @@ def create_db():
     cur.execute('''
         CREATE TABLE IF NOT EXISTS WeatherData (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            zip TEXT,
+            zip INTEGER,
             date TEXT,
             temp FLOAT,
             pressure INTEGER,
